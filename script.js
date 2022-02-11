567fcee3333f6650ddee76efdf75d2a42423b830
Abner <abnerdasilvatavares@gmail.com>
2022-02-11 16:49:28 -0300

    Versão 3.1 - input bugado

--- script.js
+++ script.js
@@ -52,81 +52,83 @@ function mostrarParticipantes(resposta) {
 }
 
 
-    function criaMenu() {
-        const criandoMenu = document.querySelector(".contatos");
-        criandoMenu.innerHTML += `<div class="usuario" onclick="selecionarContato(this, '${numeroDeUsuario}')">
+function criaMenu() {
+    const criandoMenu = document.querySelector(".contatos");
+    criandoMenu.innerHTML += `<div class="usuario" onclick="selecionarContato(this, '${numeroDeUsuario}')">
     <ion-icon name="person-circle-sharp"></ion-icon>
     <p class="${numeroDeUsuario}">${nomeDeUsuario}</p>
     <div class="checked-contatos escondido"><ion-icon name="checkmark-sharp"></ion-icon></div>
     </div>`
-    }
+}
 
 
-    function informarHorario() {
-        data = new Date();
-        hora = data.getHours();
-        minutos = data.getMinutes();
-        segundos = data.getSeconds();
-        return `${hora}:${minutos}:${segundos}`;
-    }
+function informarHorario() {
+    data = new Date();
+    hora = data.getHours();
+    minutos = data.getMinutes();
+    segundos = data.getSeconds();
+    return `${hora}:${minutos}:${segundos}`;
+}
 
 
-    function enviarMensagem() {
-        let msg = document.querySelector("input").value;
-        const mensagem = document.querySelector("section");
-        if (visibilidadeSelecionada === "Público") {
-            horario = informarHorario();
-            mensagem.innerHTML += `<div class="normal" data-identifier="message">
+function enviarMensagem() {
+    let msg = document.querySelector("input").value;
+    const mensagem = document.querySelector("section");
+    if (visibilidadeSelecionada === "Público") {
+        horario = informarHorario();
+        mensagem.innerHTML += `<div class="normal" data-identifier="message">
         <p> <time>(${horario})</time> <strong class="nome">${nomeDeUsuario}</strong> para <strong>${selecionado}:</strong> ${msg}</p>
         </div>`
-            msg.value = "";
-        } else if (visibilidadeSelecionada === "Reservadamente") {
-            horario = informarHorario();
-            mensagem.innerHTML += `<div class="reservada" data-identifier="message">
+        msg.value = "";
+    } else if (visibilidadeSelecionada === "Reservadamente") {
+        horario = informarHorario();
+        mensagem.innerHTML += `<div class="reservada" data-identifier="message">
         <p> <time>(${horario})</time> <strong class="nome">${nomeDeUsuario}</strong> reservadamente para <strong>${selecionado}:</strong> ${msg}</p>
         </div>`
-        }
+        const input = document.querySelector("input");
+        input.value = "";
     }
+}
 
 
-    function entrarNaSala(resposta) {
-        const section = document.querySelector("section");
-        horario = informarHorario();
+function entrarNaSala(resposta) {
+    const section = document.querySelector("section");
+    horario = informarHorario();
 
-        section.innerHTML += `<div class="entrou">
+    section.innerHTML += `<div class="entrou">
     <p> <time>(${horario})</time> <strong>${nomeDeUsuario}</strong> entra na sala...</p>
     </div>`;
-        numeroDeUsuario++;
+    numeroDeUsuario++;
 
-        const verificacao = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", { name: nomeDeUsuario });
-        setInterval(() => verificacao.then(verificarSeEstaNaSala), 5000);
-    }
+    const verificacao = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", { name: nomeDeUsuario });
+    setInterval(() => verificacao.then(verificarSeEstaNaSala), 5000);
+}
 
-    function verificarSeEstaNaSala(resposta) {
-        if (resposta.status === 200) {
-            console.log("OK");
-        }
+function verificarSeEstaNaSala(resposta) {
+    if (resposta.status === 200) {
+        console.log("OK");
     }
+}
 
-    function selecionarContato(elemento, nome) {
-        const user = document.querySelector(`.${nome}`);
-        selecionado = user.innerHTML;
-    }
+function selecionarContato(elemento, nome) {
+    const user = document.querySelector(`.${nome}`);
+    selecionado = user.innerHTML;
+}
 
-    function selecionarVisibilidade(elemento, visibilidade) {
-        const escolha = document.querySelector(`.${visibilidade}`);
-        visibilidadeSelecionada = escolha.innerHTML;
-        console.log(escolha);
-    }
+function selecionarVisibilidade(elemento, visibilidade) {
+    const escolha = document.querySelector(`.${visibilidade}`);
+    visibilidadeSelecionada = escolha.innerHTML;
+    console.log(escolha);
+}
 
-    function apareceMenu() {
-        const menu = document.querySelector("aside");
-        menu.classList.remove("escondido");
-    }
+function apareceMenu() {
+    const menu = document.querySelector("aside");
+    menu.classList.remove("escondido");
+}
 
-    function desapareceMenu() {
-        const menu = document.querySelector("aside");
-        if (menu.classList.contains("escondido") === false) {
-            menu.classList.add("escondido");
-        }
+function desapareceMenu() {
+    const menu = document.querySelector("aside");
+    if (menu.classList.contains("escondido") === false) {
+        menu.classList.add("escondido");
     }
+}
