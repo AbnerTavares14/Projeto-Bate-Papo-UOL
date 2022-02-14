630bf0e8f388977d6ca489a779ec14cc284ec473
Abner <abnerdasilvatavares@gmail.com>
2022-02-14 15:43:14 -0300

    Versão final

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let visibilidadeSelecionada = "Público";
 let selecionado = "Todos";
 let nomeDeUsuario;
-let flag = false;
 let data;
 let hora;
 let minutos;
@@ -22,15 +21,15 @@ promise.then(mostrarParticipantes);
 setInterval(() => {
     promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/participants");
     promise.then(mostrarParticipantes)
-    },10000);
+}, 10000);
 
 let promiseMensagens = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
 promiseMensagens.then(carregarMensagens);
 
-    setInterval(() => {
+setInterval(() => {
     promiseMensagens = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
     promiseMensagens.then(atualizarMensagens);
-    },10000);
+}, 3000);
 
 function deuErro(erro) {
     if (erro.response.status === 400) {
@@ -45,7 +44,7 @@ function mostrarParticipantes(resposta) {
     incrementarMenu.innerHTML = `<div class="todos" onclick="selecionarContato(this, 'nome0')">
     <ion-icon name="people-sharp"></ion-icon>
     <p class="nome0">Todos</p>
-    <div class="checked-contatos escondido"><ion-icon name="checkmark-sharp"></ion-icon></div>`;
+    <div class="checked-contatos"><ion-icon name="checkmark-sharp"></ion-icon></div>`;
     for (let i = 0; i < resposta.data.length; i++) {
         incrementarMenu.innerHTML += `<div class="usuario" onclick="selecionarContato(this, 'nome${i}')">
             <ion-icon name="person-circle-sharp"></ion-icon>
@@ -63,101 +62,80 @@ function informarHorario() {
     return `${hora}:${minutos}:${segundos}`;
 }
 
-
-// function enviarMensagemNormal(resposta) {
-//     console.log(resposta);
-//     const mensagem = document.querySelector("section");
-//     mensagem.innerHTML += `<div class="normal" data-identifier="message">
-//         <p> <time>(${horario})</time> <strong class="nome">${nomeDeUsuario}</strong> para <strong>${selecionado}:</strong> ${msg}</p>
-//         </div>`;
-//     // if (visibilidadeSelecionada === "Público") {
-//     //     horario = informarHorario();
-//     //     mensagem.innerHTML += `<div class="normal" data-identifier="message">
-//     //     <p> <time>(${horario})</time> <strong class="nome">${nomeDeUsuario}</strong> para <strong>${selecionado}:</strong> ${msg}</p>
-//     //     </div>`
-//     //     const input = document.querySelector("input");
-//     //     input.value = "";
-//     //     mensagem.scrollIntoView();
-//     // } else if (visibilidadeSelecionada === "Reservadamente") {
-//     //     horario = informarHorario();
-//     //     mensagem.innerHTML += `<div class="reservada" data-identifier="message">
-//     //     <p> <time>(${horario})</time> <strong class="nome">${nomeDeUsuario}</strong> reservadamente para <strong>${selecionado}:</strong> ${msg}</p>
-//     //     </div>`;
-//     //     const input = document.querySelector("input");
-//     //     input.value = "";
-//     //     mensagem.scrollIntoView();
-//     // }
-// }
-
-
-function enviarMensagensAoServidor(resposta){
+function enviarMensagensAoServidor(resposta) {
     let msg = document.querySelector("input").value;
-    if(visibilidadeSelecionada === "Público"){
-        const promiseEnvioMensagem = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", {from:nomeDeUsuario, to:selecionado, text:msg, type:"message"});
+    if (visibilidadeSelecionada === "Público") {
+        const promiseEnvioMensagem = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", { from: nomeDeUsuario, to: selecionado, text: msg, type: "message" });
         promiseEnvioMensagem.then(obterNovamenteMensagensdoServidor);
+        promiseEnvioMensagem.catch(erroMensagem);
         const input = document.querySelector("input");
         input.value = "";
-    }else{
-        const promiseEnvioMensagem = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", {from:nomeDeUsuario, to:selecionado, text:msg, type:"private_message"});
+    } else {
+        const promiseEnvioMensagem = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", { from: nomeDeUsuario, to: selecionado, text: msg, type: "private_message" });
         promiseEnvioMensagem.then(obterNovamenteMensagensdoServidor);
+        promiseEnvioMensagem.catch(erroMensagem);
         const input = document.querySelector("input");
         input.value = "";
     }
 
 }
 
-function obterNovamenteMensagensdoServidor(resposta){
+function erroMensagem(erro) {
+    console.log(erro.response);
+}
+
+function obterNovamenteMensagensdoServidor(resposta) {
     const promiseAtualizarMensagens = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
     promiseAtualizarMensagens.then(atualizarMensagens);
 }
 
-function carregarMensagens(resposta){
+function carregarMensagens(resposta) {
     const mensagem = document.querySelector("section");
-    mensagem.scrollIntoView();
-    for(let i = 0; i < resposta.data.length; i++){
-        if(resposta.data[i].type === "message"){
+    mensagem.lastElementChild.scrollIntoView()
+    for (let i = 0; i < resposta.data.length; i++) {
+        if (resposta.data[i].type === "message") {
             mensagem.innerHTML += `<div class="normal" data-identifier="message">
             <p> <time>(${resposta.data[i].time})</time> <strong class="nome">${resposta.data[i].from}</strong> para <strong>${resposta.data[i].to}:</strong> ${resposta.data[i].text}</p>
             </div>`;
-            mensagem.scrollIntoView();
-        }else if(resposta.data[i].type === "private_message"){
+            mensagem.lastElementChild.scrollIntoView()
+        } else if (resposta.data[i].type === "private_message") {
             mensagem.innerHTML += `<div class="reservada" data-identifier="message">
             <p> <time>(${resposta.data[i].time})</time> <strong class="nome">${resposta.data[i].from}</strong> reservadamente para <strong>${resposta.data[i].to}:</strong> ${resposta.data[i].text}</p>
             </div>`;
-            mensagem.scrollIntoView();
+            mensagem.lastElementChild.scrollIntoView()
         }
-        else if(resposta.data[i].type === "status"){
+        else if (resposta.data[i].type === "status") {
             mensagem.innerHTML += `<div class="entrou" data-identifier="message">
             <p> <time>(${resposta.data[i].time})</time> <strong class="nome">${resposta.data[i].from}</strong> ${resposta.data[i].text}</p>
             </div>`;
-            mensagem.scrollIntoView();
+            mensagem.lastElementChild.scrollIntoView()
         }
     }
 }
 
-function atualizarMensagens(resposta){
+function atualizarMensagens(resposta) {
     const mensagem = document.querySelector("section");
     mensagem.innerHTML = "";
-    for(let i = 0; i < resposta.data.length; i++){
-        if(resposta.data[i].type === "message"){
+    for (let i = 0; i < resposta.data.length; i++) {
+        if (resposta.data[i].type === "message") {
             mensagem.innerHTML += `<div class="normal" data-identifier="message">
             <p> <time>(${resposta.data[i].time})</time> <strong class="nome">${resposta.data[i].from}</strong> para <strong>${resposta.data[i].to}:</strong> ${resposta.data[i].text}</p>
             </div>`;
-            mensagem.scrollIntoView();
-        }else if(resposta.data[i].type === "private_message"){
+            mensagem.lastElementChild.scrollIntoView()
+        } else if (resposta.data[i].type === "private_message") {
             mensagem.innerHTML += `<div class="reservada" data-identifier="message">
             <p> <time>(${resposta.data[i].time})</time> <strong class="nome">${resposta.data[i].from}</strong> reservadamente para <strong>${resposta.data[i].to}:</strong> ${resposta.data[i].text}</p>
             </div>`;
-            mensagem.scrollIntoView();
+            mensagem.lastElementChild.scrollIntoView()
         }
-        else if(resposta.data[i].type === "status"){
+        else if (resposta.data[i].type === "status") {
             mensagem.innerHTML += `<div class="entrou" data-identifier="message">
             <p> <time>(${resposta.data[i].time})</time> <strong class="nome">${resposta.data[i].from}</strong> ${resposta.data[i].text}</p>
             </div>`;
-            mensagem.scrollIntoView();
+            mensagem.lastElementChild.scrollIntoView()
         }
     }
-    mensagem.scrollIntoView();
+    mensagem.lastElementChild.scrollIntoView()
 }
 
 
@@ -170,14 +148,17 @@ function entrarNaSala(resposta) {
     </div>`;
     numeroDeUsuario++;
 
-    const verificacao = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", { name: nomeDeUsuario });
-    setInterval(() => verificacao.then(verificarSeEstaNaSala), 5000);
+    let verificacao;
+    setInterval(() => {
+        verificacao = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", { name: nomeDeUsuario });
+        verificacao.then(verificarSeEstaNaSala)
+    }, 5000);
 }
 
 function verificarSeEstaNaSala(resposta) {
     if (resposta.status === 200) {
         console.log("OK");
-    }else{
+    } else {
         window.location.reload();
     }
 }
@@ -185,12 +166,29 @@ function verificarSeEstaNaSala(resposta) {
 function selecionarContato(elemento, nome) {
     const user = document.querySelector(`.${nome}`);
     selecionado = user.innerHTML;
+    const contatoSelecionado = document.querySelector(".selecionado");
+    if (contatoSelecionado === null) {
+        elemento.classList.add("selecionado");
+    } else {
+        contatoSelecionado.classList.remove("selecionado");
+        elemento.classList.add("selecionado");
+    }
 }
 
 function selecionarVisibilidade(elemento, visibilidade) {
     const escolha = document.querySelector(`.${visibilidade}`);
+    const checkReservado = document.querySelector(".checked-lock");
+    const checkPublico = document.querySelector(".checked");
     visibilidadeSelecionada = escolha.innerHTML;
-    console.log(escolha);
+    if (visibilidadeSelecionada === "Reservadamente") {
+        checkReservado.classList.remove("escondido");
+        checkPublico.classList.add("escondido");
+    } else if (visibilidadeSelecionada === "Público") {
+        if (checkPublico.classList.contains("escondido")) {
+            checkPublico.classList.remove("escondido");
+            checkReservado.classList.add("escondido");
+        }
+    }
 }
 
 function apareceMenu() {
